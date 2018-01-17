b25d9f5fd820fe00aca5718757db10f0dfc6f586
Dwight Ledet <dwightledet@gmail.com>
2018-01-17 10:11:22 +0100

    add gender question

--- questionnaire.py
+++ questionnaire.py
@@ -4,8 +4,9 @@
 name = raw_input("What is your name? ")
 age = raw_input("How old are you? ")
 country = raw_input("What country were you born in? ")
+gender = raw_input("What is your gender?")
 
 print "Name %s, Age %s, " \
-"Country %s." % (name, age, country)
+"Country %s, Gender %s." % (name, age, country, gender)
 
 
