0687647e2250a7ba575dfe159d59a31818f53d91
Dwight Ledet <dwightledet@gmail.com>
2018-01-17 10:03:30 +0100

    Initial commit

---
+++ questionnaire.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# Test Questions
+
+name = raw_input("What is your name? ")
+age = raw_input("How old are you? ")
+country = raw_input("What country were you born in? ")
+
+print "Name %s, Age %s, " \
+"Country %s." % (name, age, country)
+
+
